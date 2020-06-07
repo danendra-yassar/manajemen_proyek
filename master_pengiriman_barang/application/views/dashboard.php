31811d38f68501e323490345c599cf7af5edb983
danendra-yassar <64626805+danendra-yassar@users.noreply.github.com>
2020-06-07 21:20:51 +0700

    modif dashboard

--- master_pengiriman_barang/application/views/dashboard.php
+++ master_pengiriman_barang/application/views/dashboard.php
@@ -1,9 +1,80 @@
-<div class="content-wrapper">
-	<section class="content-header">
-	  <h1>
-		<?php echo $title?>
-	  </h1>
-	</section>
+  <!-- Small boxes (Stat box) -->
+<div class="row">
+ <div class="content-wrapper">
+  <section class="content-header">
+   <h1>
+		<p><?php echo $title?></p>
+   </h1>
+</section>
 	<section class="content">
+		<div class="col-lg-3 col-6">
+				<!-- small box -->
+				<div class="small-box bg-info">
+				<div class="inner">
+					<h3>36</h3>
+
+					<p>Barang</p>
+				</div>
+				<div class="icon">
+					<i class="ion ion-bag"></i>
+				</div>
+				<a href="index.php/barang" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+				</div>
+			</div>
+			<!-- ./col -->
+			<div class="col-lg-3 col-6">
+				<!-- small box -->
+				<div class="small-box bg-success">
+				<div class="inner">
+					<h3>3<sup style="font-size: 20px">%</sup></h3>
+
+					<p>Pengiriman</p>
+				</div>
+				<div class="icon">
+					<i class="ion ion-stats-bars"></i>
+				</div>
+				<a href="index.php/pengiriman" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+				</div>
+			</div>
+			<!-- ./col -->
+			<div class="col-lg-3 col-6">
+				<!-- small box -->
+				<div class="small-box bg-warning">
+				<div class="inner">
+					<h3>9</h3>
+
+					<p>Kurir</p>
+				</div>
+				<div class="icon">
+					<i class="ion ion-person-add"></i>
+				</div>
+				<a href="index.php/kurir" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+				</div>
+			</div>
+			<!-- ./col -->
+			<div class="col-lg-3 col-6">
+				<!-- small box -->
+				<div class="small-box bg-danger">
+				<div class="inner">
+					<h3>3</h3>
+
+					<p>Kategori</p>
+				</div>
+				<div class="icon">
+					<i class="ion ion-pie-graph"></i>
+				</div>
+				<a href="index.php/kategori" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+				</div>
+			</div>
+			<!-- ./col -->
+		</div>
 	</section>
 </div>
+
+<footer class="main-footer">
+    			<strong>Copyright &copy; MJD Express 2020</strong>
+    			All rights reserved.
+    			<div class="float-right d-none d-sm-inline-block">
+      			<b>Version</b> 0.0.1
+    			</div>
+  		</footer>
